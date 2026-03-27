7c5dc28f0a5ef896259b363a8131be30a4b3d4a5
Cesar <romeomacias.cesar@gmail.com>
2026-03-27 13:15:31 +0100

    Fix PWA paths for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'iron-cal-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/main.js',
-  '/manifest.json'
+  './',
+  'index.html',
+  'style.css',
+  'main.js',
+  'manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
